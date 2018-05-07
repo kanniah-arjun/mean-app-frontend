74de9af826868ef73b323828d80158fa8f2a9b84
Arjun Kanniah <akanniah@miraclesoft.com>
2018-05-07 16:12:16 -0400

    External IP routing

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -10,7 +10,7 @@ export class UserService {
 
   // retreiving UserService
   getUsers() {
-    return this.http.get('35.232.173.186:80/api/users')
+    return this.http.get('http://35.232.173.186:80/api/users')
       .map(res => res.json());
   }
 
@@ -18,13 +18,13 @@ export class UserService {
   addUser(newUser) {
     const headers = new Headers();
     headers.append('COntent-Type', 'application/json');
-    return this.http.post('35.232.173.186:80/api/user', newUser, {headers: headers})
+    return this.http.post('http://35.232.173.186:80/api/user', newUser, {headers: headers})
       .map(res => res.json());
   }
 
   // delete UserService
   deleteUser(id) {
-    return this.http.delete('35.232.173.186:80/api/user/' + id)
+    return this.http.delete('http://35.232.173.186:80/api/user/' + id)
       .map(res => res.json());
   }
 
