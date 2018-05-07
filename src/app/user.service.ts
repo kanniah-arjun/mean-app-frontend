c863cdf772b358dd62378c40d5437062cf3fef47
Arjun Kanniah <akanniah@miraclesoft.com>
2018-05-07 15:52:02 -0400

    Update UI Route

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -10,7 +10,7 @@ export class UserService {
 
   // retreiving UserService
   getUsers() {
-    return this.http.get('35.188.187.149:31396/api/users')
+    return this.http.get('35.188.187.149:31211/api/users')
       .map(res => res.json());
   }
 
@@ -18,13 +18,13 @@ export class UserService {
   addUser(newUser) {
     const headers = new Headers();
     headers.append('COntent-Type', 'application/json');
-    return this.http.post('35.188.187.149:31396/api/user', newUser, {headers: headers})
+    return this.http.post('35.188.187.149:31211/api/user', newUser, {headers: headers})
       .map(res => res.json());
   }
 
   // delete UserService
   deleteUser(id) {
-    return this.http.delete('35.188.187.149:31396/api/user/' + id)
+    return this.http.delete('35.188.187.149:31211/api/user/' + id)
       .map(res => res.json());
   }
 
